caf6f9e7bc16912837a3f481273484395ca7da35
LUKASRIB15 <lucas.ribeiro06@aluno.ifce.edu.br>
2024-07-18 10:13:56 -0300

    :sparkles: feat: configuration of cookies and middleware and users routes

---
+++ src/routes/users.ts
@@ -0,0 +1,61 @@
+import { FastifyInstance } from "fastify";
+import { knex } from "../database";
+import { z } from "zod";
+
+export async function usersRoutes(app:FastifyInstance){
+  app.post('/register', async(request, reply)=>{
+    const createUserBodySchema = z.object({
+      name: z.string(),
+      email: z.string().email(),
+    })
+
+    const {name, email} = createUserBodySchema.parse(request.body)
+
+    let sessionId = request.cookies.sessionId
+
+    if(!sessionId){
+      sessionId = crypto.randomUUID()
+
+      reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
+    await knex('users').insert({
+      id: crypto.randomUUID(),
+      name,
+      email,
+      session_id: sessionId
+    })
+
+    return reply.status(201).send()
+  })
+
+  app.post('/',async (request, reply)=>{
+    const loggedUserBodySchema = z.object({
+      email: z.string().email()
+    })
+
+    const {email} = loggedUserBodySchema.parse(request.body)
+
+    const user = await knex('users').where({email}).first()
+
+    if(!user){
+      return reply.status(400).send({error: "User not found"})
+    }
+
+    let sessionId = request.cookies.sessionId
+
+    if(!sessionId){
+      sessionId = crypto.randomUUID()
+
+      reply.setCookie('sessionId', sessionId, {
+        path: '/',
+        maxAge: 60 * 60 * 24 * 7, // 7 days
+      })
+    }
+
+    return reply.status(200).send()
+  })
+}
